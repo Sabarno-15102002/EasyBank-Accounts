40dc8f93b43f3d9a05581e23d7903f813c47566e
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-03-24 00:54:25 +0530

    API Documentation Enhanced

--- src/main/java/com/sabarno/accounts/AccountsApplication.java
+++ src/main/java/com/sabarno/accounts/AccountsApplication.java
@@ -4,10 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "sabarnobiwas2002@gmail.com"
+				)
+		)
+)
 public class AccountsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
 	}
--- src/main/java/com/sabarno/accounts/controller/AccountsController.java
+++ src/main/java/com/sabarno/accounts/controller/AccountsController.java
@@ -15,13 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sabarno.accounts.constants.AccountsConstants;
 import com.sabarno.accounts.dto.CustomerDto;
+import com.sabarno.accounts.dto.ErrorResponseDto;
 import com.sabarno.accounts.dto.ResponseDto;
 import com.sabarno.accounts.service.IAccountsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 
+@Tag(name = "CRUD REST APIs for Accounts in EazyBank", description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE account details")
 @RestController
 @RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
 @AllArgsConstructor
@@ -30,6 +38,11 @@ public class AccountsController {
 
   private IAccountsService iAccountsService;
 
+  @Operation(summary = "Create Account REST API", description = "REST API to create new Customer &  Account inside EazyBank")
+  @ApiResponses({
+      @ApiResponse(responseCode = "201", description = "HTTP Status CREATED"),
+      @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+  })
   @PostMapping("/accounts")
   public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
     iAccountsService.createAccount(customerDto);
@@ -38,6 +51,11 @@ public class AccountsController {
         .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
   }
 
+  @Operation(summary = "Fetch Account Details REST API", description = "REST API to fetch Customer &  Account details based on a mobile number")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+      @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+  })
   @GetMapping("/fetch")
   public ResponseEntity<CustomerDto> fetchAccountDetails(
       @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
@@ -45,6 +63,12 @@ public class AccountsController {
     return ResponseEntity.status(HttpStatus.OK).body(customerDto);
   }
 
+  @Operation(summary = "Update Account Details REST API", description = "REST API to update Customer &  Account details based on a account number")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+      @ApiResponse(responseCode = "417", description = "Expectation Failed"),
+      @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+  })
   @PutMapping("/update")
   public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
     boolean isUpdated = iAccountsService.updateAccount(customerDto);
@@ -59,6 +83,12 @@ public class AccountsController {
     }
   }
 
+  @Operation(summary = "Delete Account & Customer Details REST API", description = "REST API to delete Customer &  Account details based on a mobile number")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+      @ApiResponse(responseCode = "417", description = "Expectation Failed"),
+      @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+  })
   @DeleteMapping("/delete")
   public ResponseEntity<ResponseDto> deleteAccountDetails(
       @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
