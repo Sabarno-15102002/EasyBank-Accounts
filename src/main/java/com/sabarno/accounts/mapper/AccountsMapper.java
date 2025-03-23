0baee036aa6cb9bc6f7af7f19027c39d2bd46f4c
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-03-23 12:26:41 +0530

    EasyBank Accounts Microservice Created

---
+++ src/main/java/com/sabarno/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,20 @@
+package com.sabarno.accounts.mapper;
+
+import com.sabarno.accounts.dto.AccountsDto;
+import com.sabarno.accounts.entity.Accounts;
+
+public class AccountsMapper {
+   public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
---
+++ src/main/java/com/sabarno/accounts/service/IAccountsService.java
@@ -0,0 +1,14 @@
+package com.sabarno.accounts.service;
+
+import com.sabarno.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+  void createAccount(CustomerDto customerDto);
+
+  CustomerDto fetchAccount(String mobileNumber);
+
+  boolean updateAccount(CustomerDto customerDto);
+
+  boolean deleteAccount(String mobileNumber);
+}
