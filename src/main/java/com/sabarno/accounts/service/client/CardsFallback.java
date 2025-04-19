4457d7fad155a2ea05203a453fba829bf89ef3eb
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-04-19 23:59:35 +0530

    Added FallBack services

---
+++ src/main/java/com/sabarno/accounts/service/client/CardsFallback.java
@@ -0,0 +1,15 @@
+package com.sabarno.accounts.service.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import com.sabarno.accounts.dto.CardsDto;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
