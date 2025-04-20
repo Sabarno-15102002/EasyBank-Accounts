a761ce250c154ddd3bf5c698aa4c9fa231539e00
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-04-20 17:43:32 +0530

    Added ratelimiting feature

--- src/main/java/com/sabarno/accounts/controller/AccountsController.java
+++ src/main/java/com/sabarno/accounts/controller/AccountsController.java
@@ -25,6 +25,7 @@ import com.sabarno.accounts.dto.ErrorResponseDto;
 import com.sabarno.accounts.dto.ResponseDto;
 import com.sabarno.accounts.service.IAccountsService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -149,12 +150,18 @@ public class AccountsController {
       @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
       @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
   })
-  @GetMapping("/java-version")
-  public ResponseEntity<String> getJavaVersion() {
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(environment.getProperty("JAVA_HOME"));
-  }
+  @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
+    }
 
   @Operation(summary = "Get Contact Info", description = "Contact Info details that can be reached out in case of any issues")
   @ApiResponses({
